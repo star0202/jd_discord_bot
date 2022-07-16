91ba5be4f1298157e658e883f72cec014dc54cb1
star0202_dev <stardev.uwu@gmail.com>
2022-07-16 22:02:27 +0900

    Edit bot class system

--- main.py
+++ main.py
@@ -7,23 +7,29 @@ from auth import token
 from config import STATUS
 import utils.logger
 
-bot = commands.Bot(command_prefix="v!", intents=discord.Intents.all(), help_command=None)
-bot.start_time = time.time()
-bot.load_extension('jishaku')
+
 utils.logger.setup_logging()
 logger = logging.getLogger("main")
 
 
-@bot.event
-async def on_ready():
-    logger.info(f"Logged in as {bot.user.name}")
-    await bot.change_presence(
-        status=discord.Status.online,
-        activity=discord.Game(STATUS),
-    )
-for filename in os.listdir("functions"):
-    if filename.endswith(".py"):
-        bot.load_extension(f"functions.{filename[:-3]}")
-logger.info(f"{len(bot.extensions)} extensions are completely loaded")
+class Viridian(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix="v!", intents=discord.Intents.all(), help_command=None)
+        for filename in os.listdir("functions"):
+            if filename.endswith(".py"):
+                self.load_extension(f"functions.{filename[:-3]}")
+        logger.info(f"{len(self.extensions)} extensions are completely loaded")
+
+    async def on_ready(self):
+        logger.info(f"Logged in as {bot.user.name}")
+        await self.change_presence(
+            status=discord.Status.online,
+            activity=discord.Game(STATUS),
+        )
+
+
+bot = Viridian()
+bot.start_time = time.time()
+bot.load_extension('jishaku')
 
 bot.run(token)
